b0dfe3c0155d5802d9f19acb626c2c72a293f307
hideonfate <103841006+hideonfate@users.noreply.github.com>
2025-04-22 18:34:19 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,14 +1,14 @@
+import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+
 const OPENAI_API_HOST = "openrouter.ai/api";
 
-Deno.serve(async (request) => {
+serve(async (request) => {
   const url = new URL(request.url);
-  url.host = OPENAI_API_HOST;
 
-  const newRequest = new Request(url.toString(), {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: "follow",
-  });
-  return await fetch(newRequest);
+  if (url.pathname === "/") {
+    return fetch(new URL("./Readme.md", import.meta.url));
+  }
+
+  url.host = OPENAI_API_HOST;
+  return await fetch(url, request);
 });
