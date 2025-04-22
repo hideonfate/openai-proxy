8a801244094b07b7bca5cd24553e02b62a3ef7d9
hideonfate <103841006+hideonfate@users.noreply.github.com>
2025-04-22 21:39:51 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
-const OPENAI_API_HOST = "www.hideonfate.top";
+const OPENAI_API_HOST = "us.ifopen.ai";
 
 serve(async (request) => {
   const url = new URL(request.url);
